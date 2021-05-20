fa42788bf5e2239706e52e7b01b34d2037e552ef
Guillaume Ferrari <57795522+gferrari89@users.noreply.github.com>
2021-05-20 09:58:13 +0200

    test

---
+++ pipeline_jenkins/pipeline_wordpress_data_web.groovy
@@ -0,0 +1,92 @@
+pipeline {
+
+  agent any
+
+options {
+ansiColor("xterm")
+}
+
+stages {
+
+    stage ('Clonage repo GIT') {
+      steps {
+      checkout([
+                $class: 'GitSCM',
+                branches: [[name: '*/develop']],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [],
+                submoduleCfg: [],
+                userRemoteConfigs: [[credentialsId: 'git', url: 'https://github.com/gferrari89/tp_wordpress.git']]
+              ])
+      }
+    }
+
+    stage ('ANSIBLE LINT') {
+      steps {
+          sh 'echo "test ansible-lint"'
+      }
+    }
+
+    stage ('DEV : Installation des produits') {
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'install_produit_data_web.yml',
+          inventory: 'inventories/dev/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
+    }
+    stage ('DEV : Deploiement Wordpress') {
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'deploy_wordpress_data_web.yml',
+          inventory: 'inventories/dev/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
+    }
+
+    stage ('Test Selenium') {
+      steps {
+          sh 'echo "test selenium"'
+      }
+    }
+
+/*
+    stage ('PROD : Installation des produits') {
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'install_produit_data_web.yml',
+          inventory: 'inventories/prod/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
+    }
+    stage ('PROD : Deploiement Wordpress') {
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'deploy_wordpress_data_web.yml',
+          inventory: 'inventories/prod/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
+    }
+*/
+  }
+}
